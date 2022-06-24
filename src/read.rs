e36e2d7ea3753cd4a6b1caaa3f989db60de92792
Marty B <mrbm74@gmail.com>
2022-06-24 10:19:17 +0200

    Add Clone to FileMetadata

--- src/read.rs
+++ src/read.rs
@@ -41,7 +41,7 @@ pub enum CompressionMethod {
 
 /// Metadata for a file or directory in the archive,
 /// retrieved from its central directory
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct FileMetadata<'a> {
     /// Uncompressed size of the file in bytes
     pub size: usize,
